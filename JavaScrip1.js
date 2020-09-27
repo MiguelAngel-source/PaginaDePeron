b0571ddebf38b629a28a38c523683314c12fe9bc
MiguelAngel-source <58918569+MiguelAngel-source@users.noreply.github.com>
2020-09-26 20:57:52 -0500

    Add files via upload

---
+++ JavaScrip1.js
@@ -0,0 +1,7 @@
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+// document.getElementById("date").innerHTML = 10;
+
+document.getElementById("date").innerHTML="hola";
